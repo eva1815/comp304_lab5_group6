6f8d372bae65ff7c67970918445e20188827ddb7
eva1815 <109616875+eva1815@users.noreply.github.com>
2023-04-12 16:01:24 -0400

    start up lab6 application

---
+++ app/src/main/java/com/example/group6/alexeva_comp304sec304_lab5_group6/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.group6.alexeva_comp304sec304_lab5_group6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
